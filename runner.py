faba1012301bf058bd94c08214b7f7f438bdfe20
stas-home <stasijanka13@gmail.com>
2023-08-15 21:41:21 +0300

    reformat code with single responsibility principle

--- runner.py
+++ runner.py
@@ -5,63 +5,73 @@ from typing import Type
 import pygame
 
 from apple import generate_apple
-from constants import HEIGHT, WIDTH
+from constants import HEIGHT, VECTOR_CHOICES, WIDTH
 from snake import Snake
 from snake_collision import apple_collision, wall_collision
 from square import Shape, Triangle
 from utils import generate_position
 
+running = True
 
-def game(obj: Type[Shape]):
+
+def game_setup():
     # pygame setup
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Snake")
-
     clock = pygame.time.Clock()
-    running = True
 
-    vector_choice = ("RIGHT", "LEFT", "DOWN", "UP")
+    return screen, clock
 
-    snake = Snake(
-        obj_type=obj, position=generate_position(), vector=random.choice(vector_choice)
+
+def generate_snake(obj: Type[Shape]):
+    return Snake(
+        obj_type=obj, position=generate_position(), vector=random.choice(VECTOR_CHOICES)
     )
 
-    snake_speed = 0
-    snake_speed_limit = 20
+
+def manage_keyboard(snake: Snake):
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    global running
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                if snake.vector != "DOWN":
+                    snake.vector = "UP"
+            if event.key == pygame.K_DOWN:
+                if snake.vector != "UP":
+                    snake.vector = "DOWN"
+            if event.key == pygame.K_RIGHT:
+                if snake.vector != "LEFT":
+                    snake.vector = "RIGHT"
+            if event.key == pygame.K_LEFT:
+                if snake.vector != "RIGHT":
+                    snake.vector = "LEFT"
+
+
+def game(obj: Type[Shape]):
+    global running
+    screen, clock = game_setup()
+
+    snake = generate_snake(obj)
 
     apple = generate_apple(obj, snake.tail)
 
     while running and snake.alive:
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    if snake.vector != "DOWN":
-                        snake.vector = "UP"
-                if event.key == pygame.K_DOWN:
-                    if snake.vector != "UP":
-                        snake.vector = "DOWN"
-                if event.key == pygame.K_RIGHT:
-                    if snake.vector != "LEFT":
-                        snake.vector = "RIGHT"
-                if event.key == pygame.K_LEFT:
-                    if snake.vector != "RIGHT":
-                        snake.vector = "LEFT"
-
-        snake_speed += 1
-        if snake_speed > snake_speed_limit:
+        manage_keyboard(snake)
+        snake.speed += 1
+        if snake.speed > snake.speed_limit:
             snake.move()
             if wall_collision(snake):
                 snake.alive = False
             if apple_collision(apple, snake):
                 snake.eat()
                 apple = generate_apple(obj, snake.tail)
-                snake_speed_limit -= 1
-            snake_speed = 0
+                snake.speed_limit -= 1
+            snake.speed = 0
 
         # fill the screen with a color to wipe away anything from last frame
 
